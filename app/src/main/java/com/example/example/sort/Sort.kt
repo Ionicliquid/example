0cd3e3f5e281f6e6bdb6292679dfa0be9e9061fe
qiao-li <>
2019-11-29 17:03:59 +0800

    排序算法

--- app/src/main/java/com/example/example/sort/Sort.kt
+++ app/src/main/java/com/example/example/sort/Sort.kt
@@ -12,7 +12,7 @@ class Sort {
             }
 
             for (i in array.indices) {
-                for (j in 0..array.size - 1 - i) {
+                for (j in 0 until array.size - 1 - i) {
                     if (array[j + 1] < array[j]) {
                         val temp = array[j + 1]
                         array[j + 1] = array[j]
@@ -50,9 +50,9 @@ class Sort {
                 return array
             }
             var current: Int
-            for (i in 0 until array.size) {
-                current = array[i + 1]
-                var preIndex = i
+            for (i in array.indices) {
+                current = array[i]
+                var preIndex = i - 1
                 while (preIndex >= 0 && current < array[preIndex]) {
                     array[preIndex + 1] = array[preIndex]
                     preIndex--
@@ -65,24 +65,120 @@ class Sort {
         // 希尔排序
 
         fun shellSort(array: Array<Int>): Array<Int> {
-            val len = array.size / 2
-            var temp = len / 2
-            var gap = temp
+            var len = array.size
+            var temp: Int
+            var gap = len / 2
             while (gap > 0) {
                 for (i in gap until len) {
                     temp = array[i]
                     var preIndex = i - gap
                     while (preIndex >= 0 && array[preIndex] > temp) {
+                        array[preIndex + gap] = array[preIndex]
                         preIndex -= gap
                     }
-                    array[preIndex+gap] = temp
+                    array[preIndex + gap] = temp
                 }
-                gap/=2
+
+                gap /= 2
             }
-            return  array
+            return array
+
 
         }
 
+        // 归并排序
+        fun mergeSort(array: Array<Int>): Array<Int> {
+            if (array.size < 2) {
+                return array
+            }
+            var mid = array.size / 2
+            var left = mergeSort(array.copyOfRange(0, mid))
+            var right = mergeSort(array.copyOfRange(mid, array.size))
+
+            return merge(left, right)
+        }
+
+        private fun merge(left: Array<Int>, right: Array<Int>): Array<Int> {
+            var result = Array(left.size + right.size) { i->i}
+            var i = 0
+            var j = 0
+            for (index in result.indices) {
+                when {
+                    i >= left.size -> result[index] = right[j++]
+                    j >= right.size -> result[index] = left[i++]
+                    left[i] > right[j] -> result[index] = right[j++]
+                    else -> result[index] = left[i++]
+                }
+            }
+            return result
+
+        }
+
+        // 快速排序
+        // 堆排序
+        private var arraySize: Int = 0
+
+        fun headSort(array: Array<Int>): Array<Int> {
+            arraySize = array.size
+            if (arraySize < 1) {
+                return array
+            }
+            for (i in arraySize / 2 - 1 downTo 0) {
+                adjustHeap(array, i)
+            }
+//            10 ,9 ,7 ,8 ,6 ,3 ,5 ,0 ,2 ,1 ,
+//            6, 5, 7, 0, 9, 3, 8, 10, 2, 1
+
+            while (arraySize > 0) {
+                swap(array, 0, arraySize - 1)
+                arraySize--
+                adjustHeap(array, 0)
+            }
+            return array
+
+
+        }
+
+
+        fun adjustHeap(array: Array<Int>, i: Int) {
+            var maxIndex = i
+            if (i * 2 < arraySize && array[i * 2] > array[maxIndex]) {
+                maxIndex = i * 2
+            }
+            if (i * 2 + 1 < arraySize && array[i * 2 + 1] > array[maxIndex]) {
+                maxIndex = i * 2 + 1
+            }
+
+            if (maxIndex != i) {
+                swap(array, maxIndex, i)
+                adjustHeap(array, maxIndex)
+            }
+        }
+
+        fun swap(array: Array<Int>, i: Int, j: Int) {
+            var temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+        }
+
+
+        // 计数排序
+        // 桶排序
+        // 基数排序
+
+    }
+
+
+}
+
+fun main(arg: Array<String>) {
+    var array = arrayOf(6, 5, 7, 0, 9, 3, 8, 10, 2, 1)
+//    3, 5, 7, 0, 1, 6, 8, 10, 2, 9
+//    1, 0, 2, 5, 3, 6, 7, 9, 8, 10
+//    0, 1, 2, 3, 5, 6, 7, 8, 9, 10,
+
+    for (item in Sort.mergeSort(array)) {
+        System.out.print("$item ,")
     }
 
 }
