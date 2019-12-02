93071da2ce48fd3ca86f99421929d0a072c53dd6
qiao-li <>
2019-12-02 16:41:54 +0800

    基数排序

--- app/src/main/java/com/example/example/okhttp/Call.kt
+++ app/src/main/java/com/example/example/okhttp/Call.kt
@@ -1,5 +1,6 @@
 package com.example.example.okhttp
 
+import okhttp3.Callback
 import okhttp3.Request
 import okhttp3.Response
 import okio.Timeout
--- app/src/main/java/com/example/example/sort/Sort.kt
+++ app/src/main/java/com/example/example/sort/Sort.kt
@@ -115,6 +115,40 @@ class Sort {
         }
 
         // 快速排序
+
+        fun quickSort(array: Array<Int>, start: Int, end: Int): Array<Int> {
+            if (array.isEmpty() || start < 0 || end >= array.size || start > end) {
+                return array
+            }
+            var smallIndex = partition(array, start, end)
+            if (smallIndex > start) {
+                quickSort(array, start, smallIndex - 1)
+
+            }
+            if (smallIndex < end) {
+                quickSort(array, smallIndex + 1, end)
+            }
+            return array
+
+        }
+
+        private fun partition(array: Array<Int>, start: Int, end: Int): Int {
+            var pivot: Int = ((start + Math.random() * (end - start + 1)).toInt())
+            var smallIndex = start - 1
+            swap(array, pivot, end)
+            for (i in start..end) {
+                if (array[i] <= array[end]) {
+                    smallIndex++
+                    if (i > smallIndex) {
+                        swap(array, i, smallIndex)
+                    }
+                }
+
+            }
+            System.out.print("pivot:$pivot smallIndex:$smallIndex,\n")
+            return smallIndex
+        }
+
         // 堆排序
         private var arraySize: Int = 0
 
@@ -140,7 +174,7 @@ class Sort {
         }
 
 
-        fun adjustHeap(array: Array<Int>, i: Int) {
+        private fun adjustHeap(array: Array<Int>, i: Int) {
             var maxIndex = i
             if (i * 2 < arraySize && array[i * 2] > array[maxIndex]) {
                 maxIndex = i * 2
@@ -155,7 +189,7 @@ class Sort {
             }
         }
 
-        fun swap(array: Array<Int>, i: Int, j: Int) {
+        private fun swap(array: Array<Int>, i: Int, j: Int) {
             var temp = array[i]
             array[i] = array[j]
             array[j] = temp
@@ -201,7 +235,89 @@ class Sort {
         }
 
         // 桶排序
+        fun bucketSort(array: ArrayList<Int>, bucketSize: Int): ArrayList<Int> {
+            if (array == null || array.size < 2) {
+                return array
+            }
+            var tempBucketSize = bucketSize
+            var max = array[0]
+            var min = array[0]
+            for (i in array.indices) {
+                if ((array[i] > max)) {
+                    max = array[i]
+                }
+                if (array[i] < min) {
+                    min = array[i]
+                }
+            }
+            var bucketCount = (max - min) / tempBucketSize + 1
+            var bucketArray = ArrayList<ArrayList<Int>>(bucketCount)
+            var resultArr = ArrayList<Int>()
+            for (i in 0 until bucketCount) {
+                bucketArray.add(ArrayList())
+            }
+            for (i in array.indices) {
+                bucketArray[(array[i] - min) / tempBucketSize].add(array[i])
+            }
+            for (i in 0 until bucketCount) {
+                if (tempBucketSize == 1) {
+                    for (j in 0 until bucketArray[i].size) {
+                        resultArr.add(bucketArray[i][j])
+                    }
+                } else {
+                    if (bucketCount == 1) {
+                        tempBucketSize--
+                    }
+                    var temp = bucketSort(bucketArray[i], tempBucketSize)
+                    for (j in temp.indices) {
+                        resultArr.add(temp[j])
+                    }
+                }
+            }
+            return resultArr
+
+        }
+
         // 基数排序
+        fun radixSort(array: Array<Int>): Array<Int> {
+            if (array == null || array.size < 2) {
+                return array
+            }
+            var max = array[0]
+            for (i in array.indices) {
+                max = Math.max(max, array[i])
+            }
+            var maxDigit = 0
+            while (max != 0) {
+                max /= 10
+                maxDigit++
+            }
+            var mod = 10
+            var div = 1
+            var bucketList = ArrayList<ArrayList<Int>>()
+            for (i in 0 until 10) {
+                bucketList.add(ArrayList())
+            }
+            for (i in 0 until maxDigit) {
+                if (i != 0) {
+                    mod *= 10
+                    div *= 10
+                }
+                for (j in array.indices) {
+                    val num = (array[j] % mod) / div
+                    bucketList[num].add(array[j])
+                }
+                var index = 0
+                for (j in bucketList.indices) {
+                    for (k in bucketList[j].indices) {
+                        array[index++] = bucketList[j][k]
+                    }
+                    bucketList[j].clear()
+                }
+            }
+            return array
+
+        }
 
     }
 
@@ -214,8 +330,15 @@ fun main(arg: Array<String>) {
 //    1, 0, 2, 5, 3, 6, 7, 9, 8, 10
 //    0, 1, 2, 3, 5, 6, 7, 8, 9, 10,
 
-    for (item in Sort.coutingSort(array)) {
-        System.out.print("$item ,")
+    var arrayList: ArrayList<Int> = ArrayList()
+    arrayList.addAll(array.toList())
+//    for (i in Sort.bucketSort(arrayList, 2)) {
+//        System.out.print("$i, ")
+//    }
+
+    for (i in Sort.radixSort(array)) {
+        System.out.print("$i, ")
+
     }
 
 }
